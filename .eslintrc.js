c5dfa41cae1ff97a15ca70b5b0c0b5692f0bc933
Jordan Hill <74479504+Jordanhill90@users.noreply.github.com>
2023-12-18 11:17:02 -0500

    try again

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   plugins: ["react", "jest"],
   rules: {
-    indent: ["error", 2],
+    indent: ["error", 4],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
     semi: ["error", "never"],
